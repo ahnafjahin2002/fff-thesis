c159acd06ff394c146a48c5595de7910faad390d
Kafee <kafeebin@gmail.com>
2026-06-08 00:17:55 +0600

    feat: add landing page feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ReadingPage from './pages/ReadingPage';
 import DashboardPage from './pages/DashboardPage';
+import LandingPage from './pages/LandingPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<DashboardPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/reading" element={<ReadingPage />} />
       </Routes>
     </BrowserRouter>
